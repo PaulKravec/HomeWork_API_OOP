94ee0df68492f2f9b0d14786e591ebd9bb023769
Paul_RazDva <paul1312white@gmail.com>
2022-10-06 14:46:28 +0300

    Initial commit

---
+++ src/main/java/ru/netology/API/Post.java
@@ -0,0 +1,12 @@
+package ru.netology.API;
+
+public class Post {
+    public String name;
+    public String surname;
+    public String patrynamic;
+    public String phone;
+    public String passport;
+    public boolean subscription;
+    public FormDate birthday;
+
+}
